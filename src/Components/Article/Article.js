e3e36fc79eb947c4444a19d5d82011964a522020
Imtiaz Ahmed <imtiazahmed026@gmail.com>
2021-10-19 17:40:23 +0600

    article card setup done

---
+++ src/Components/Article/Article.js
@@ -0,0 +1,54 @@
+//this component will show all the articles one by one
+import React from 'react';
+import { Card, CardGroup, Col, Container, Row } from 'react-bootstrap';
+import './Article.css'
+const Article = (props) => {
+    const { title, description, Author, date, views, img } = (props.article);
+    return (
+        <>
+
+            <Col lg={4} xs={12} className="">
+
+                <CardGroup className="">
+                    <Card className="article-card shadow-sm pt-3 my-5">
+                        {/* https://img.icons8.com/external-kiranshastry-lineal-kiranshastry/64/000000/external-date-medical-kiranshastry-lineal-kiranshastry.png */}
+                        {/* <Card.Img variant="top" src="" /> */}
+                        {/* their parent div */}
+                        <div className="d-flex justify-content-between align-items-center ">
+                            {/* this div for data  */}
+                            <div className="d-flex justify-content-center align-items-center">
+                                <img src="https://img.icons8.com/external-kiranshastry-lineal-kiranshastry/64/000000/external-date-medical-kiranshastry-lineal-kiranshastry.png" alt="data" className="mx-2 w-25"></img>
+                                <h4 className="me-1">{date}</h4>
+                            </div>
+                            {/* this div for views */}
+                            <div className="d-flex justify-content-center align-items-center">
+                                <img src="https://img.icons8.com/ios-glyphs/64/000000/visible.png" alt="views" className="me-2 w-25" />
+                                <h4 className="me-2">{views}</h4>
+                            </div>
+                        </div>
+                        <Card.Body>
+                            <Card.Title className="article-card-title"><h3>{title}</h3></Card.Title>
+                            <Card.Text className="article-card-description">
+                                <h6>{description}</h6>
+                            </Card.Text>
+                        </Card.Body>
+                        <Card.Footer className="card-footer d-flex justify-content-center align-items-center">
+                            {/* this div is for user icon */}
+                            <div className="">
+                                <img src={img} alt="user"></img>
+                            </div>
+                            {/* this div is for user name */}
+                            <div className="d-flex ms-3 mt-1  ">
+                                <h5> {Author}</h5>
+                            </div>
+                        </Card.Footer>
+                    </Card>
+                </CardGroup>
+
+            </Col>
+
+        </>
+    );
+};
+
+export default Article;
--- src/Components/Articles/Articles.js
+++ src/Components/Articles/Articles.js
@@ -1,7 +1,21 @@
 //this component will keep all the available articles
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { Row } from 'react-bootstrap';
+import Article from '../Article/Article';
 
 const Articles = () => {
+    const [articles, setArticles] = useState([]);
+    const [showAllArticles, setShowAllArticles] = useState(false);
+    useEffect(() => {
+        fetch("./doconlinedb.json")
+            .then(res => res.json())
+            .then(data => setArticles(data[2]));
+    },
+        []
+    )
+    const handleShowAll = () => {
+        setShowAllArticles(true);
+    }
     return (
         <>
             <div className="articles-heading text-center">
@@ -11,6 +25,21 @@ const Articles = () => {
                 </h5>
             </div>
             <div className="articles-container">
+                <Row>
+                    {
+                        showAllArticles ? articles.map(article => <Article article={article} key={article.id}>
+                            {/* <button onClick={handleShowAll}></button> */}
+                        </Article>
+                        ) :
+                            articles.slice(0, 3).map(article => <Article article={article} key={article.id}>
+
+                            </Article>)
+                    }
+                </Row>
+                {
+                    showAllArticles ||
+                    <button className="text-center" onClick={handleShowAll}> More Articles</button>
+                }
 
             </div>
         </>
