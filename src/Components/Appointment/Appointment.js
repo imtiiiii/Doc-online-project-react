6e6215aaaf745f3f3e500891597e0b43710a921b
Imtiaz Ahmed <imtiazahmed026@gmail.com>
2021-10-20 13:00:57 +0600

    doing dynamic route

--- src/Components/Appointment/Appointment.js
+++ src/Components/Appointment/Appointment.js
@@ -1,6 +1,8 @@
 import React, { useEffect, useState } from 'react';
+import { Container, Form, Button, Row, Col } from 'react-bootstrap';
 import { useParams } from 'react-router';
-
+import './Appointment.css';
+import bookImg from '../../img/book1.jpg'
 const Appointment = () => {
     /* 
         1. lets get all available booking/servic that a user can try to book
@@ -29,15 +31,58 @@ const Appointment = () => {
             break;
         }
     }
-    // 11. Finally show it to the user
+    //11.Stop from reloading whe user submits his response
+
+
+    // 12. Finally show it to the user
 
 
 
 
 
     return (
-        <div>
-            <h1>Hi im dynamic {id}</h1>
+        <div >
+            <Container className="book-this-service-container d-flex flex-column justify-content-center align-items-center">
+                <div className="bg-danger p-5 rounded-circle d-flex flex-column justify-content-center align-items-center">
+                    <div className="speciality-img">
+                        <img src={service[0]?.img} alt="speciality" className="rounded-pill"></img>
+                    </div>
+                    <div >
+                        <h3 className="fw-bolder">{service[0]?.name}</h3>
+                    </div>
+                    <div >
+                        <h4 className="fw-bolder">Price range: ${service[0]?.price}</h4>
+                    </div>
+                </div>
+                <h1 className="text-center my-5 fw-bold">Book Now</h1>
+                <Row className=" w-100 d-flex  justify-content-center align-items-center ">
+                    <Col lg={4} className="book-img my-5">
+                        <img src={bookImg} alt="book"></img>
+                    </Col>
+                    <Col lg={4} className="">
+                        <Form className="my-5">
+                            <Form.Group className="mb-3" controlId="formBasicEmail">
+                                <Form.Label >Email address</Form.Label>
+                                <Form.Control className="my-2" type="email" placeholder="Enter email" />
+                                <Form.Label>Your Contact Number</Form.Label>
+                                <Form.Control type="phone" className="my-2" placeholder="Phone Number" />
+                                <Form.Label>Address</Form.Label>
+                                <Form.Control type="input" className="my-2" placeholder="Adress" />
+                                <Form.Label>Age</Form.Label>
+                                <Form.Control type="number" className="my-2" placeholder="Age" />
+                            </Form.Group>
+                            {/* <Form.Group className="mb-3" controlId="formBasicCheckbox">
+                            <Form.Check type="checkbox" label="Check me out" />
+                        </Form.Group> */}
+                            <Button variant="primary" type="submit">
+                                Book
+                            </Button>
+                        </Form>
+
+                    </Col>
+                </Row>
+            </Container>
+
         </div>
     );
 };
